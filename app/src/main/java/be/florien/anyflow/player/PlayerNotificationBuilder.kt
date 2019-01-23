02a133cb68d681726dce57833e8c9ece807c36a2
Florien Flament <florien@movify.be>
2019-01-23 13:15:25 +0100

    #75

---
+++ app/src/main/java/be/florien/anyflow/player/PlayerNotificationBuilder.kt
@@ -0,0 +1,137 @@
+package be.florien.anyflow.player
+
+import android.app.PendingIntent
+import android.app.Service
+import android.graphics.Bitmap
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaButtonReceiver
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import be.florien.anyflow.R
+import be.florien.anyflow.extension.GlideApp
+import be.florien.anyflow.persistence.local.model.Song
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
+
+class PlayerNotificationBuilder(
+        private val service: Service,
+        private val pendingIntent: PendingIntent,
+        private val mediaSession: MediaSessionCompat) {
+
+    private var state: State = State(null, false, false, false)
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+    private lateinit var metadataBuilder: MediaMetadataCompat.Builder
+
+    private val skipToPreviousAction by lazy {
+        NotificationCompat.Action(
+                android.R.drawable.ic_media_previous,
+                service.getString(R.string.app_name),
+                MediaButtonReceiver.buildMediaButtonPendingIntent(service, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS))
+    }
+    private val playAction by lazy {
+        NotificationCompat.Action(
+                android.R.drawable.ic_media_play,
+                service.getString(R.string.app_name),
+                MediaButtonReceiver.buildMediaButtonPendingIntent(service, PlaybackStateCompat.ACTION_PLAY_PAUSE))
+    }
+    private val pauseAction by lazy {
+        NotificationCompat.Action(
+                android.R.drawable.ic_media_pause,
+                service.getString(R.string.app_name),
+                MediaButtonReceiver.buildMediaButtonPendingIntent(service, PlaybackStateCompat.ACTION_PLAY_PAUSE))
+    }
+    private val skipToNextAction by lazy {
+        NotificationCompat.Action(
+                android.R.drawable.ic_media_next,
+                service.getString(R.string.app_name),
+                MediaButtonReceiver.buildMediaButtonPendingIntent(service, PlaybackStateCompat.ACTION_SKIP_TO_NEXT))
+    }
+
+    fun updateNotification(song: Song, isPlaying: Boolean, hasPrevious: Boolean, hasNext: Boolean) {
+        val newState = State(song, isPlaying, hasPrevious, hasNext)
+
+        if (newState == state) {
+            return
+        }
+        val shouldChangeForegroundState = state.shouldUpdateForeground(newState)
+        state = newState
+        notificationBuilder = NotificationCompat.Builder(service, PlayerService.MEDIA_SESSION_NAME)
+                .setContentTitle(service.getString(R.string.notification_title_artist, song.title, song.artistName))
+                .setContentText(song.albumName)
+                .setContentIntent(pendingIntent)
+                .setOnlyAlertOnce(true)
+                .setSmallIcon(R.drawable.notif)
+                .setColor(ContextCompat.getColor(service, R.color.primary))
+
+        var playPauseIndex = 0
+        if (hasPrevious) {
+            notificationBuilder.addAction(skipToPreviousAction)
+            ++playPauseIndex
+        }
+        notificationBuilder.addAction(if (isPlaying) pauseAction else playAction)
+        if (hasNext) {
+            notificationBuilder.addAction(skipToNextAction)
+        }
+        notificationBuilder.setStyle(android.support.v4.media.app.NotificationCompat.MediaStyle()
+                .setMediaSession(mediaSession.sessionToken)
+                .setShowActionsInCompactView(playPauseIndex))
+
+        metadataBuilder = MediaMetadataCompat.Builder()
+                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, song.artistName)
+                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ARTIST, song.albumArtistName)
+                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, song.albumName)
+                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, song.title)
+                .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, song.time.toLong())
+                .putString(MediaMetadataCompat.METADATA_KEY_GENRE, song.genre)
+        GlideApp
+                .with(service)
+                .asBitmap()
+                .load(song.art)
+                .listener(asyncBitmapLoadingReceiver)
+                .submit()
+        notifyChange(shouldChangeForegroundState)
+    }
+
+    private fun notifyChange(updateForegroundState: Boolean) {
+        mediaSession.setMetadata(metadataBuilder.build())
+        val notification = notificationBuilder.build()
+        if (updateForegroundState) {
+            if (state.isPlaying) {
+                service.startForeground(1, notification)
+            } else {
+                service.stopForeground(false)
+                NotificationManagerCompat.from(service).notify(1, notification)
+            }
+        } else {
+            NotificationManagerCompat.from(service).notify(1, notification)
+        }
+    }
+
+    /**
+     * Inner classes
+     */
+
+    private val asyncBitmapLoadingReceiver = object : RequestListener<Bitmap> {
+        override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+            notificationBuilder.setLargeIcon(resource)
+            metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, resource)
+            notifyChange(false)
+            return true
+        }
+
+        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+            return true
+        }
+    }
+
+    private data class State(val song: Song?, val isPlaying: Boolean, val hasPrevious: Boolean, val hasNext: Boolean) {
+        fun shouldUpdateForeground(newState: State) = newState.isPlaying != isPlaying
+    }
+}
--- app/src/main/java/be/florien/anyflow/player/PlayerService.kt
+++ app/src/main/java/be/florien/anyflow/player/PlayerService.kt
@@ -3,23 +3,13 @@ package be.florien.anyflow.player
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Binder
 import android.os.IBinder
-import android.support.v4.app.NotificationCompat
-import android.support.v4.app.NotificationManagerCompat
-import android.support.v4.content.ContextCompat
-import android.support.v4.media.MediaMetadataCompat
-import android.support.v4.media.app.NotificationCompat.MediaStyle
 import android.support.v4.media.session.MediaButtonReceiver
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import be.florien.anyflow.AnyFlowApp
-import be.florien.anyflow.R
-import be.florien.anyflow.extension.GlideApp
 import be.florien.anyflow.persistence.local.model.Song
-import com.bumptech.glide.request.target.SimpleTarget
-import com.bumptech.glide.request.transition.Transition
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -52,40 +42,23 @@ class PlayerService : Service() {
         PendingIntent.getActivity(this@PlayerService, 0, intent, 0)
     }
     private lateinit var mediaSession: MediaSessionCompat
-
-    private var lastSong: Song? = null
-
+    private lateinit var notificationBuilder: PlayerNotificationBuilder
     private val playBackStateBuilder: PlaybackStateCompat.Builder = PlaybackStateCompat.Builder()
             .setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE or
                     PlaybackStateCompat.ACTION_SKIP_TO_NEXT or
                     PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
             )
-    private val skipToPreviousAction by lazy {
-        NotificationCompat.Action(
-                android.R.drawable.ic_media_previous,
-                this.getString(R.string.app_name),
-                MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS))
-    }
-    private val playAction by lazy {
-        NotificationCompat.Action(
-                android.R.drawable.ic_media_play,
-                this.getString(R.string.app_name),
-                MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE))
-    }
-    private val pauseAction by lazy {
-        NotificationCompat.Action(
-                android.R.drawable.ic_media_pause,
-                this.getString(R.string.app_name),
-                MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_PLAY_PAUSE))
-    }
-    private val skipToNextAction by lazy {
-        NotificationCompat.Action(
-                android.R.drawable.ic_media_next,
-                this.getString(R.string.app_name),
-                MediaButtonReceiver.buildMediaButtonPendingIntent(this, PlaybackStateCompat.ACTION_SKIP_TO_NEXT))
-    }
 
     private var subscription: Disposable? = null
+
+    private val isPlaying
+        get() = playerController.isPlaying()
+
+    private val hasNext
+        get() = playingQueue.listPosition < (playingQueue.itemsCount - 1)
+
+    private val hasPrevious
+        get() = playingQueue.listPosition > 0
 
     /**
      * Lifecycle
@@ -122,6 +95,8 @@ class PlayerService : Service() {
         }
         setPlaybackState(PlaybackStateCompat.STATE_NONE, 0L)
 
+        notificationBuilder = PlayerNotificationBuilder(this, pendingIntent, mediaSession)
+
         subscription = Flowable.combineLatest<PlayerController.State, Song?, Pair<PlayerController.State, Song?>>(
                 playerController.stateChangeNotifier,
                 playingQueue.currentSongUpdater,
@@ -131,7 +106,7 @@ class PlayerService : Service() {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnNext {
-                    if (!playerController.isPlaying()) {
+                    if (!isPlaying) {
                         stopForeground(false)
                     }
                     val playbackState = when (it.first) {
@@ -144,14 +119,8 @@ class PlayerService : Service() {
 
                     setPlaybackState(playbackState, 0)
                     it.second?.let { song ->
-                        updateNotification(song)
-                        GlideApp
-                                .with(this)
-                                .asBitmap()
-                                .load(song.art)
-                                .into(AsyncBitmapLoadingReceiver(song))
+                        notificationBuilder.updateNotification(song, isPlaying, hasPrevious, hasNext)
                     }
-                    lastSong = it.second
                 }
                 .subscribe()
     }
@@ -172,12 +141,6 @@ class PlayerService : Service() {
      * Inner classes
      */
 
-    inner class AsyncBitmapLoadingReceiver(val song: Song) : SimpleTarget<Bitmap>() {
-        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-            updateNotification(song, resource)
-        }
-    }
-
     inner class LocalBinder : Binder() {
         val service: PlayerController
             get() = playerController
@@ -192,50 +155,7 @@ class PlayerService : Service() {
         mediaSession.setPlaybackState(playBackStateBuilder.build())
     }
 
-    private fun updateNotification(song: Song, albumArt: Bitmap? = null) {
-        val notificationBuilder = NotificationCompat.Builder(this, MEDIA_SESSION_NAME)
-                .setContentTitle(getString(R.string.notification_title_artist, song.title, song.artistName))
-                .setContentText(song.albumName)
-                .setContentIntent(pendingIntent)
-                .setOnlyAlertOnce(true)
-                .setSmallIcon(R.drawable.notif)
-                .setColor(ContextCompat.getColor(this, R.color.primary))
-
-        var playPauseIndex = 0
-        if (playingQueue.listPosition > 0) {
-            notificationBuilder.addAction(skipToPreviousAction)
-            ++playPauseIndex
-        }
-        notificationBuilder.addAction(if (playerController.isPlaying()) pauseAction else playAction)
-        if (playingQueue.listPosition < playingQueue.itemsCount - 1) {
-            notificationBuilder.addAction(skipToNextAction)
-        }
-        notificationBuilder.setStyle(MediaStyle()
-                .setMediaSession(mediaSession.sessionToken)
-                .setShowActionsInCompactView(playPauseIndex))
-
-        val metadataBuilder = MediaMetadataCompat.Builder()
-                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, song.artistName)
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ARTIST, song.albumArtistName)
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, song.albumName)
-                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, song.title)
-                .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, song.time.toLong())
-                .putString(MediaMetadataCompat.METADATA_KEY_GENRE, song.genre)
-
-        if (albumArt != null) {
-            notificationBuilder.setLargeIcon(albumArt)
-            metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, albumArt)
-        }
-        mediaSession.setMetadata(metadataBuilder.build())
-        val notification = notificationBuilder.build()
-        if (playerController.isPlaying()) {
-            startForeground(1, notification)
-        } else {
-            NotificationManagerCompat.from(this).notify(1, notification)
-        }
-    }
-
     companion object {
-        private const val MEDIA_SESSION_NAME = "AnyFlow"
+        const val MEDIA_SESSION_NAME = "AnyFlow"
     }
 }
