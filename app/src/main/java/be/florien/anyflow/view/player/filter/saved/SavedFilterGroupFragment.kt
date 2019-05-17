a9750d4b595274a99eee6a87265f0517e4c80041
Florien Flament <florien@movify.be>
2019-05-17 17:56:54 +0200

    Contextual action mode

--- app/src/main/java/be/florien/anyflow/view/player/filter/saved/SavedFilterGroupFragment.kt
+++ app/src/main/java/be/florien/anyflow/view/player/filter/saved/SavedFilterGroupFragment.kt
@@ -2,9 +2,9 @@ package be.florien.anyflow.view.player.filter.saved
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
 import androidx.databinding.Observable
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,11 +18,33 @@ import be.florien.anyflow.view.player.filter.BaseFilterVM
 
 class SavedFilterGroupFragment : BaseFilterFragment() {
 
+    private var singleActionMode: ActionMode? = null
     private lateinit var vm: SavedFilterGroupVM
     override val baseVm: BaseFilterVM
         get() = vm
 
     private lateinit var binding: FragmentSavedFilterGroupBinding
+    private var selectedList = mutableListOf<Int>() //todo selection in vm ?
+
+    private val actionModeCallback = object : ActionMode.Callback {
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            val inflater: MenuInflater = mode.menuInflater
+            inflater.inflate(R.menu.menu_filter, menu)
+            return true
+        }
+
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            return when (item.itemId) {
+                else -> false
+            }
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu) = false
+
+        override fun onDestroyActionMode(mode: ActionMode) {
+            singleActionMode = null
+        }
+    }
 
     override fun getTitle(): String = getString(R.string.filter_title_saved)
 
@@ -33,7 +55,7 @@ class SavedFilterGroupFragment : BaseFilterFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentSavedFilterGroupBinding.inflate(inflater, container, false)
-        binding.savedList.layoutManager = GridLayoutManager(requireContext(), 3, RecyclerView.VERTICAL, false)
+        binding.savedList.layoutManager = GridLayoutManager(requireContext(), 2, RecyclerView.VERTICAL, false)
         binding.savedList.adapter = FilterGroupAdapter()
         vm.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
             override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
@@ -46,21 +68,40 @@ class SavedFilterGroupFragment : BaseFilterFragment() {
 
     inner class FilterGroupViewHolder(
             container: ViewGroup,
-            private val binding: ItemFilterGroupBinding = ItemFilterGroupBinding.inflate(LayoutInflater.from(container.context), container, false))
-        : RecyclerView.ViewHolder(binding.root) {
+            private val itemBinding: ItemFilterGroupBinding = ItemFilterGroupBinding.inflate(LayoutInflater.from(container.context), container, false))
+        : RecyclerView.ViewHolder(itemBinding.root) {
 
         init {
-            binding.root.setOnClickListener {
-                vm.changeForSavedGroup(adapterPosition)
+            itemBinding.root.setOnClickListener {
+                if (selectedList.isEmpty()) {
+                    vm.changeForSavedGroup(adapterPosition)
+                } else toggleSelection()
+            }
+            itemBinding.root.setOnLongClickListener {
+                if (singleActionMode != null) {
+                    toggleSelection()
+                } else {
+                    singleActionMode = (requireActivity() as AppCompatActivity).startSupportActionMode(actionModeCallback)
+                    toggleSelection()
+                }
+                true
             }
         }
 
-        fun bind(filterGroup: FilterGroup, coverUrls: List<String>) {
-            binding.filterGroup = filterGroup
-            binding.cover1Url = coverUrls[0]
-            binding.cover2Url = coverUrls[1]
-            binding.cover3Url = coverUrls[2]
-            binding.cover4Url = coverUrls[3]
+        private fun toggleSelection() {
+            if (!selectedList.remove(adapterPosition)) {
+                selectedList.add(adapterPosition)
+            }
+            binding.savedList.adapter?.notifyItemChanged(adapterPosition)
+        }
+
+        fun bind(filterGroup: FilterGroup, coverUrls: List<String>, isSelected: Boolean) {
+            itemBinding.filterGroup = filterGroup
+            itemBinding.cover1Url = coverUrls[0]
+            itemBinding.cover2Url = coverUrls[1]
+            itemBinding.cover3Url = coverUrls[2]
+            itemBinding.cover4Url = coverUrls[3]
+            itemBinding.isSelected = isSelected
         }
     }
 
@@ -70,7 +111,7 @@ class SavedFilterGroupFragment : BaseFilterFragment() {
         override fun getItemCount(): Int = vm.filterGroups.size
 
         override fun onBindViewHolder(holder: FilterGroupViewHolder, position: Int) {
-            holder.bind(vm.filterGroups[position], vm.imageForGroups[position])
+            holder.bind(vm.filterGroups[position], vm.imageForGroups[position], selectedList.contains(position)) // todo don't change images, only selection
         }
 
     }
