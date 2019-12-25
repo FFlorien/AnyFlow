08a34fa470748f43a1598bcda5bd478df1b6f279
florien <florienflament@gmail.com>
2019-12-25 16:55:56 +0100

    Nicer display for item options.

--- app/src/main/java/be/florien/anyflow/persistence/server/AmpacheApi.kt
+++ app/src/main/java/be/florien/anyflow/persistence/server/AmpacheApi.kt
@@ -27,7 +27,7 @@ interface AmpacheApi {
     @GET("server/xml.server.php")
     fun getSongs(
             @Query("action") action: String = "songs",
-            @Query("update") update: String = "1970-01-01",
+            @Query("add") add: String = "1970-01-01",
             @Query("auth") auth: String,
             @Query("limit") limit: Int,
             @Query("offset") offset: Int)
@@ -36,7 +36,7 @@ interface AmpacheApi {
     @GET("server/xml.server.php")
     fun getArtists(
             @Query("action") action: String = "artists",
-            @Query("update") update: String = "1970-01-01",
+            @Query("add") add: String = "1970-01-01",
             @Query("auth") auth: String,
             @Query("limit") limit: Int,
             @Query("offset") offset: Int)
@@ -45,7 +45,7 @@ interface AmpacheApi {
     @GET("server/xml.server.php")
     fun getAlbums(
             @Query("action") action: String = "albums",
-            @Query("update") update: String = "1970-01-01",
+            @Query("add") add: String = "1970-01-01",
             @Query("auth") auth: String,
             @Query("limit") limit: Int,
             @Query("offset") offset: Int)
@@ -54,7 +54,7 @@ interface AmpacheApi {
     @GET("server/xml.server.php")
     fun getTags(
             @Query("action") action: String = "tags",
-            @Query("update") update: String = "1970-01-01",
+            @Query("add") add: String = "1970-01-01",
             @Query("auth") auth: String,
             @Query("limit") limit: Int,
             @Query("offset") offset: Int)
@@ -63,7 +63,7 @@ interface AmpacheApi {
     @GET("server/xml.server.php")
     fun getPlaylists(
             @Query("action") action: String = "playlists",
-            @Query("update") update: String = "1970-01-01",
+            @Query("add") add: String = "1970-01-01",
             @Query("auth") auth: String,
             @Query("limit") limit: Int,
             @Query("offset") offset: Int)
