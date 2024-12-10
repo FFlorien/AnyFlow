c1625470b6f75468d07d608716a5f15c0db69bde
FFlorien <florienflament@gmail.com>
2024-12-10 09:52:43 +0100

    Handle hours for long format

--- common/common-utils/src/main/java/be/florien/anyflow/common/utils/TimeOperations.kt
+++ common/common-utils/src/main/java/be/florien/anyflow/common/utils/TimeOperations.kt
@@ -56,8 +56,17 @@ object TimeOperations {
                     s.getDurationString(R.plurals.seconds_component)
         }
 
-    fun toMediaDuration(timeInSeconds: Int) =
-        String.format("%d:%02d", timeInSeconds / 60, timeInSeconds % 60)
+    fun toMediaDuration(timeInSeconds: Int) = if (timeInSeconds < (60 * 60)) {
+        String.format(Locale.getDefault(), "%d:%02d", timeInSeconds / 60, timeInSeconds % 60)
+    } else {
+        String.format(
+            Locale.getDefault(),
+            "%d:%02d:%02d",
+            timeInSeconds / (60 * 60),
+            (timeInSeconds / 60) % 60,
+            timeInSeconds % 60
+        )
+    }
 
     interface CurrentTimeUpdater {
         fun getCurrentTimeUpdated(current: Calendar): Calendar
